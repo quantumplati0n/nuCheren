68a8fdfcc82f0471294b04273d4564cb2382fe7a
quantumplati0n <akaxiacruz@lbl.gov>
2014-10-13 11:30:26 -0700

    Update NCDetectorConstruction.hh

--- include/NCDetectorConstruction.hh
+++ include/NCDetectorConstruction.hh
@@ -1,11 +1,11 @@
-#ifndef NSDetectorConstruction_h
-#define NSDetectorConstruction_h 1
+#ifndef NCDetectorConstruction_h
+#define NCDetectorConstruction_h 1
 
 #include "G4VUserDetectorConstruction.hh"
 
 class G4PhysicalVolume;
 
-class NSDetectorConstruction : public G4VUserDetectorConstruction
+class NCDetectorConstruction : public G4VUserDetectorConstruction
 {
 public:
   virtual G4VPhysicalVolume* Construct();
