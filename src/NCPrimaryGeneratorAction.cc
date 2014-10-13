79bdb313e0cebeb6959410a4f070614432e91121
quantumplati0n <akaxiacruz@lbl.gov>
2014-10-13 15:43:25 -0700

    Update NCPrimaryGeneratorAction.cc

--- src/NCPrimaryGeneratorAction.cc
+++ src/NCPrimaryGeneratorAction.cc
@@ -1,13 +1,13 @@
-#include "NCPrimaryGeneratorAction.hh"
+#include "NSPrimaryGeneratorAction.hh"
 
 #include "G4GeneralParticleSource.hh"
 
-NCPrimaryGeneratorAction::NCPrimaryGeneratorAction()
+NSPrimaryGeneratorAction::NSPrimaryGeneratorAction()
 {
   fParticleGun = new G4GeneralParticleSource();
 }
 
-void NCPrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
+void NSPrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
 {
   fParticleGun->GeneratePrimaryVertex(event);
 }
